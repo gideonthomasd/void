46cfc90d39344502b60cdd544ad9e8f9d73c21ad
gideonthomasd <56291811+gideonthomasd@users.noreply.github.com>
2021-11-26 21:29:05 +0000

    Add files via upload

--- dwm-flexipatch/config.h
+++ dwm-flexipatch/config.h
@@ -881,8 +881,8 @@ static Key keys[] = {
 	{ MODKEY|ControlMask,           XK_j,          pushdown,               {0} },
 	{ MODKEY|ControlMask,           XK_k,          pushup,                 {0} },
 	#endif // PUSH_PATCH / PUSH_NO_MASTER_PATCH
-	{ MODKEY,                       XK_i,          incnmaster,             {.i = +1 } },
-	{ MODKEY,                       XK_d,          incnmaster,             {.i = -1 } },
+	/*{ MODKEY,                       XK_i,          incnmaster,             {.i = +1 } }, */
+	/*{ MODKEY,                       XK_d,          incnmaster,             {.i = -1 } }, */
 	#if FLEXTILE_DELUXE_LAYOUT
 	{ MODKEY|ControlMask,           XK_i,          incnstack,              {.i = +1 } },
 	{ MODKEY|ControlMask,           XK_u,          incnstack,              {.i = -1 } },
